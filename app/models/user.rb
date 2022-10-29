d7f06037ad19d13619294932c17d7c757fc6aceb
savanna chiang <savannaac@gmail.com>
2022-10-28 23:17:34 -0500

    creates user and villager models and associations, adds validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :villagers, dependent: :destroy
+  # delete user, then delete user's villagers
+
+  validates_presence_of :username
+  validates_uniqueness_of :email
 end
--- app/models/villager.rb
+++ app/models/villager.rb
@@ -1,2 +1,5 @@
 class Villager < ApplicationRecord
+    belongs_to :user
+
+    validates_presence_of :img_url
 end
