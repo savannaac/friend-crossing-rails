bc4b403454d75ea64b1b342a8cde2a0e6dd1f11f
savanna chiang <savannaac@gmail.com>
2022-10-28 22:41:02 -0500

    sets up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "application#index"
+
+  resources :users do
+    resources :villagers
+  end
 end
