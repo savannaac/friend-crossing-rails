ddb433c0a55702e48b32685b302d19e6de1f9f5d
savanna chiang <savannaac@gmail.com>
2022-10-31 01:57:31 -0500

    adjusts some more for heroku

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -55,7 +55,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       root_path
     end
   
-    The path used after sign up for inactive accounts.
+    # The path used after sign up for inactive accounts.
     def after_inactive_sign_up_path_for(resource)
       super(resource)
     end
