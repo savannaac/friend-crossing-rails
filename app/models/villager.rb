106e78ae7c2290b299d01ad6251024da0941bf7d
savanna chiang <savannaac@gmail.com>
2022-10-30 22:30:18 -0500

    finishes villager new page

--- app/models/villager.rb
+++ app/models/villager.rb
@@ -2,4 +2,9 @@ class Villager < ApplicationRecord
     belongs_to :user
 
     validates_presence_of :img_url
+
+    PERSONALITY_CHOICES = ["♂ lazy", "♂ jock", "♂ cranky", "♂ smug", "♀ normal", "♀ peppy", "♀ snooty", "♀ big sister"]
+    SPECIES_CHOICES = ["alligator/crocodile", "bear/cub", "bird", "bull/cow", "deer/reindeer", "dog", "frog", "horse", "octopus", "ostrich", "robot", "special", "other"]
+    GENDER = ["male", "female"]
+    STATUS_CHOICES = ["♥ fav", "⭑ want", "✓ have", "✗ moved out", "↯ dislike", "☠ hate"]
 end
